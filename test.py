12d06ad83862cc879a80931327ad59d8eb961808
Jeremiah Marks <Jeremiah@jlmarks.org>
2015-03-02 21:43:03 -0700

    including examples

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: jeremiah.marks
+# @Date:   2015-03-02 14:01:03
+# @Last Modified by:   jeremiah.marks
+# @Last Modified time: 2015-03-02 19:25:23
+# from:  http://krondo.com/?p=1209
