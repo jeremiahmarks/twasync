adc58e7fec5e1a00659d4b593d3f152cf7bbaf33
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-03-03 21:48:40 -0700

    First working example using the LoopingCall

--- p3e2.py
+++ p3e2.py
@@ -3,7 +3,7 @@
 # @Author: jeremiah.marks
 # @Date:   2015-03-03 18:26:58
 # @Last Modified 2015-03-03
-# @Last Modified time: 2015-03-03 21:23:07
+# @Last Modified time: 2015-03-03 21:48:00
 
 
 #Part3 exercise 2
@@ -29,51 +29,86 @@
     ## twisted.internet.task.LoopingCall.start(self, interval, now=True)
       ## interval -- The number of seconds between calls. May be less than one. Precision will depend on the underlying platform, the available hardware, and the load on the system.
       ## now -- If True, run this call right now. Otherwise, wait until the interval has elapsed before beginning.
-
-
+############################################################
+## second attempt
+############################################################
 import time
 from twisted.internet.task import LoopingCall
 
 class loopingCounter(object):
+    activeCounters=0
 
     def __init__(self, counterStart, name, delayTime):
-        self.complete=False
         self.counter, self.name, self.delayTime = (counterStart, name, delayTime)
         self.starttime=time.time()
+        loopingCounter.activeCounters+=1
         self.lc=LoopingCall(self.count)
+        self.lc.start(self.delayTime)
 
     def count(self):
-        if self.counter == 0:
-            self.complete = True
+        if self.counter == 1:
+            loopingCounter.activeCounters-=1
             self.lc.stop()
             print "%s just finished its last cycle.  It ran for a total of %f seconds"%(self.name, time.time()-self.starttime)
+            if loopingCounter.activeCounters==0: reactor.stop()
         else:
             self.counter -= 1
             print "%s just ticked and has %i ticks left. delay is %f"%(self.name, self.counter, self.delayTime)
 
-
-class counterHolder(object):
-
-    def __init__(self, *args):
-        self.allCountdowns=[loopingCounter(counterStart, name, delayTime) for counterStart, name, delayTime in args]
-        self.activeCounters = (1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
-
-    def startTheLoop(self):
-        for eachCounter in self.allCountdowns:
-            eachCounter.lc.start(eachCounter.delayTime)
-            self.theActualLoop(eachCounter)
-    def theActualLoop(self,countDownTimer):
-        if not countDownTimer.complete:
-            countDownTimer.count()
-            self.activeCounters=sum(1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
-            if self.activeCounters==0: reactor.stop()
-        else:
-            self.activeCounters=sum(1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
-            print "%s just finished!\t\t%i"%(countDownTimer.name, self.activeCounters)
-            if self.activeCounters==0: reactor.stop()
-
 if __name__ == '__main__':
     from twisted.internet import reactor
-    groupOfCountdowns = counterHolder([10,"a",1.5],[2,"b", 15],[15,"c", 0.7])
-    reactor.callWhenRunning(groupOfCountdowns.startTheLoop)
+    groupOfCountdowns=[]
+    for each in ([10,"a",1.5],[2,"b", 15],[15,"c", 0.7]):
+        groupOfCountdowns.append(loopingCounter(each[0], each[1], each[2]))
     reactor.run()
+
+############################################################
+## first attempt
+############################################################
+# import time
+# from twisted.internet.task import LoopingCall
+
+# class loopingCounter(object):
+
+#     def __init__(self, counterStart, name, delayTime):
+#         self.complete=False
+#         self.counter, self.name, self.delayTime = (counterStart, name, delayTime)
+#         self.starttime=time.time()
+#         self.lc=LoopingCall(self.count)
+
+#     def count(self):
+#         if self.counter == 0:
+#             self.complete = True
+#             self.lc.stop()
+#             print "%s just finished its last cycle.  It ran for a total of %f seconds"%(self.name, time.time()-self.starttime)
+#         else:
+#             self.counter -= 1
+#             print "%s just ticked and has %i ticks left. delay is %f"%(self.name, self.counter, self.delayTime)
+
+
+# class counterHolder(object):
+
+#     def __init__(self, *args):
+#         self.allCountdowns=[loopingCounter(counterStart, name, delayTime) for counterStart, name, delayTime in args]
+#         self.activeCounters = (1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
+
+#     def startTheLoop(self):
+#         for eachCounter in self.allCountdowns:
+#             eachCounter.lc.start(eachCounter.delayTime)
+#             self.theActualLoop(eachCounter)
+#     def theActualLoop(self,countDownTimer):
+#         if not countDownTimer.complete:
+#             countDownTimer.count()
+#             self.activeCounters=sum(1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
+#             if self.activeCounters==0: reactor.stop()
+#         else:
+#             self.activeCounters=sum(1 for eachCounter in self.allCountdowns if eachCounter.complete==False)
+#             print "%s just finished!\t\t%i"%(countDownTimer.name, self.activeCounters)
+#             if self.activeCounters==0: reactor.stop()
+
+# if __name__ == '__main__':
+#     from twisted.internet import reactor
+#     groupOfCountdowns = counterHolder([10,"a",1.5],[2,"b", 15],[15,"c", 0.7])
+#     reactor.callWhenRunning(groupOfCountdowns.startTheLoop)
+#     reactor.run()
+
