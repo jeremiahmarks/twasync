e409f4ba2db3ac526dadbbca74932960beb0d60c
Jeremiah Marks <Jeremiah@jlmarks.org>
2015-03-16 21:02:35 -0700

    a file to start servers

--- utility_serverStarter.py
+++ utility_serverStarter.py
@@ -6,11 +6,15 @@
 # @Last Modified time: 2015-03-04 23:48:45
 
 import subprocess
+import os
+import sys
+import shlex
 
 ports = [14002, 14004, 14006]
 bytesizes=[50,40,20]
 poetry=['ecstasy.txt',  'fascination.txt',  'science.txt']
-pathToSampleCode="~/twasync/jdavisp3-twisted-intro-8f9d137"
+myroot=os.getcwd()
+#pathToSampleCode="~/twasync/jdavisp3-twisted-intro-8f9d137"
 
 serverVars=zip(ports,poetry,bytesizes)
 ######
@@ -23,23 +27,24 @@ def killServer(poemName):
 
 def startServers():
     global serverVars
-    global pathToSampleCode
-    startString="python %s/blocking-server/slowpoetry.py --port %i %s/poetry/%s --num-bytes %i &"
+    global myroot
+    startString="%s '%s/jdavisp3-twisted-intro-8f9d137/blocking-server/slowpoetry.py' --port %i '%s/jdavisp3-twisted-intro-8f9d137/poetry/%s' --num-bytes %i &"
     for eachDataSet in serverVars:
-        try:
-            subprocess.call(startString%(pathToSampleCode, eachDataSet[0], pathToSampleCode, eachDataSet[1], eachDataSet[2]), shell=True)
-        except OSError:
-            killServer(eachDataSet[1])
-            try:
-                subprocess.call(startString%(eachDataSet[0], eachDataSet[1], eachDataSet[2]), shell=True)
-            except OSError:
-                print "I tried, but I cannot start a server for %s on port %i"%(eachDataSet[1], eachDataSet[0])
-        except socket.error:
-            killServer(eachDataSet[1])
-            try:
-                subprocess.call(startString%(eachDataSet[0], eachDataSet[1], eachDataSet[2]), shell=True)
-            except socket.error:
-                print "I tried, but I cannot start a server for %s on port %i"%(eachDataSet[1], eachDataSet[0])
+        subprocess.Popen(shlex.split(startString%(sys.executable, myroot, eachDataSet[0], myroot, eachDataSet[1], eachDataSet[2])), shell=False)
+        # try:
+        #     subprocess.call(startString%(sys.executable, myroot, eachDataSet[0], myroot, eachDataSet[1], eachDataSet[2]), shell=False)
+        # except OSError:
+        #     killServer(eachDataSet[1])
+        #     try:
+        #         subprocess.call(startString%(sys.executable, myroot, eachDataSet[0], myroot, eachDataSet[1], eachDataSet[2]), shell=False)
+        #     except OSError:
+        #         print "I tried, but I cannot start a server for %s on port %i"%(eachDataSet[1], eachDataSet[0])
+        # except socket.error:
+        #     killServer(eachDataSet[1])
+        #     try:
+        #         subprocess.call(startString%(sys.executable, myroot, eachDataSet[0], myroot, eachDataSet[1], eachDataSet[2]), shell=False)
+        #     except socket.error:
+        #         print "I tried, but I cannot start a server for %s on port %i"%(eachDataSet[1], eachDataSet[0])
 
 if __name__ == '__main__':
     for pname in poetry:
